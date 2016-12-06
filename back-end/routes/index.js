d96cecd3e25d28875d1f98a0ce9514aca20b9451
mikeadossi <mikeadossi@gmail.com>
2016-12-05 16:15:24 -0800

    create route for adding new task

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var Task = require('../database/Task');
 
 /* GET home page. */
 router.get('/', (request, response, next) => {
   response.status(200).json({ status: 'Success',
                               message: 'SUCCESS!!!'})
 });
+
+router.post('/task', Task.add)
 
 module.exports = router;
